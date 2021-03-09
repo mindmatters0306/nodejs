941b6032622d1db9e972f50e1f6f5dfaad7c93a5
EC2 Default User <ec2-user@ip-10-0-0-186.us-west-2.compute.internal>
2021-03-09 20:55:29 +0000

    updated

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Hello Springworks!!')
 })
-app.listen(3000, () => {
-    console.log('Server is up on 3000')
+app.listen(8080, () => {
+    console.log('Server is up on 8080')
 })
